a9758b78134bed7ffa5a62b5d74a866730a029ee
JiminyKroket <50643200+JiminyKroket@users.noreply.github.com>
2019-10-17 02:54:09 -0500

    Major Overhaul
    JsFour table integrated. Remove items on use and give back key added.

--- server/main.lua
+++ server/main.lua
@@ -1,33 +1,38 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-
-ESX.RegisterUsableItem('suppressor', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)	
-    TriggerClientEvent('eden_accesories:suppressor', source)
-
+local ESX = nil
+ 
+TriggerEvent('esx:getSharedObject', function( obj ) ESX = obj end)
+ 
+ESX.RegisterUsableItem('suppressor', function( source )
+	local _source  = source
+	local xPlayer  = ESX.GetPlayerFromId(_source)
+    TriggerClientEvent('eden_accesories:use', source, 'suppressor')
+	xPlayer.removeInventoryItem('suppressor', 1)
+end)
+ 
+ESX.RegisterUsableItem('flashlight', function( source )
+	local _source  = source
+	local xPlayer  = ESX.GetPlayerFromId(_source)
+    TriggerClientEvent('eden_accesories:use', source, 'flashlight')
+	xPlayer.removeInventoryItem('flashlight', 1)
+end)
+ 
+ESX.RegisterUsableItem('grip', function( source )
+	local _source  = source
+	local xPlayer  = ESX.GetPlayerFromId(_source)
+    TriggerClientEvent('eden_accesories:use', source, 'grip')
+	xPlayer.removeInventoryItem('grip', 1)
+end)
+ 
+ESX.RegisterUsableItem('yusuf', function( source )
+	local _source  = source
+	local xPlayer  = ESX.GetPlayerFromId(_source)
+    TriggerClientEvent('eden_accesories:use', source, 'skin')
+	xPlayer.removeInventoryItem('yusuf', 1)
 end)
 
-ESX.RegisterUsableItem('flashlight', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)	
-	
-    TriggerClientEvent('eden_accesories:flashlight', source)
-	
+RegisterServerEvent('eden_accesories:giveBack')
+AddEventHandler('eden_accesories:giveBack', function(item)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	xPlayer.addInventoryItem(item, 1)
 end)
-
-ESX.RegisterUsableItem('grip', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-		
-    TriggerClientEvent('eden_accesories:grip', source)
-	
-end)
-
-
-ESX.RegisterUsableItem('yusuf', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-
-    TriggerClientEvent('eden_accesories:yusuf', source)
-
-end)
-
