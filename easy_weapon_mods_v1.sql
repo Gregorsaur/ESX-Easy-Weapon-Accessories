b4c0a9996981489444ef6d31555da5f40ed928e5
JiminyKroket <50643200+JiminyKroket@users.noreply.github.com>
2019-10-17 02:52:17 -0500

    Add attachments to RobsLiquor
    Ease of installment, if you have somewhere else for them yay you.

--- easy_weapon_mods_v1.sql
+++ easy_weapon_mods_v1.sql
@@ -1,4 +1,11 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`) VALUES ('yusuf', 'Weapon Skin', 1, 0, 0, 25);
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`) VALUES ('grip', 'Grip', 1, 0, 0, 25);
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`) VALUES ('flashlight', 'Flashlight', 1, 0, 0, 25);
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`) VALUES ('suppressor', 'Suppressor', 1, 0, 0, 25);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('yusuf', 'Weapon Skin', 1, 0, 1),
+('grip', 'Grip', 1, 0, 1),
+('flashlight', 'Flashlight', 1, 0, 1),
+('suppressor', 'Suppressor', 1, 0, 1);
+
+INSERT INTO `shops` ( `store`, `item`, `price`) VALUES
+( 'RobsLiquor', 'yusuf', 400),
+( 'RobsLiquor', 'grip', 250),
+( 'RobsLiquor', 'flashlight', 60),
+( 'RobsLiquor', 'suppressor', 750);
